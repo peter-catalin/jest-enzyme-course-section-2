5458312ad8ee62741a8ea35cf3ff2fc1b6787387
Peter Catalin <peter.catalin@lendismart.com>
2019-02-24 22:15:35 +0100

    Finishing section 2 of the course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter: 0,
+    error: false
+  };
+
+  handleIncrementButtonClick = () => {
+    this.setState({ counter: this.state.counter + 1, error: false });
+  };
+
+  handleResetClick = () => {
+    this.setState({ counter: 0, error: false });
+  };
+
+  handleDecrementButtonClick = () => {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      /*
+       The data-test attribute will be used by out test to assert that the component did actually render
+       It is a good practice to use data atrributes instead of standard HTML attributes like id or class
+       because those attributes might be changed by another person to accomplish some functionality.
+
+       By using a data attribute called data-test we make it quite clear that this attribute should not be modified
+       because it is beeing used for testing.
+       */
+      <div data-test="component-app" className="App">
+        <div data-test="counter-display" className="CounterText">
+          <h3>{`You've clicked the button ${this.state.counter} times`}</h3>
+        </div>
+
+        {this.state.error && (
+          <div data-test="error-element" className="ErrorMessage">
+            <p> The counter can't go below 0</p>
+          </div>
+        )}
+        <div className="ButtonsContainer">
+          <button data-test="increment-button" className="Button" onClick={this.handleIncrementButtonClick}>
+            Increment
+          </button>
+
+          <button data-test="decrement-button" className="Button" onClick={this.handleDecrementButtonClick}>
+            Decrement
+          </button>
+
+          <button data-test="reset-button" className="Button" onClick={this.handleResetClick}>
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,179 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// This is a really basic manner of configuring Enzyme. It could be setup globally with Jest
+// when we have multiple test files but for now this is good enough.
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a ShallowWrapper component for the App component
+ * @function setup
+ *
+ * @param {object} props - Component props specific to this setup
+ * @param {object} state  - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+};
+
+/**
+ * Returns a ShallowWrapper containing node(s) with the fiven data-test value.
+ *
+ * @param {ShallowWrapper} wrapper  - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test atrribute for search.
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
+/*
+ * The following tests are quite simple and just check if the components end up beeing rendered.
+ * We use data attributes to assert if the component is beeing rendered or not.
+ */
+test('Renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+
+test('Renders increment button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+
+test('Renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('Renders reset button', () => {
+  const wrapper = setup();
+  const resetButton = findByTestAttr(wrapper, 'reset-button');
+  expect(resetButton.length).toBe(1);
+});
+
+/**
+ * This is the first test that works with the state as well.
+ *
+ */
+test('The counter starts at 0', () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+
+/**
+ * We will actually test if the display is modified because we want our test to be as far
+ * removed from the actual implementation.
+ */
+test('Clicking the button increments the counter display', () => {
+  // Setup the component with a counter value that is different from the initial one, say 9
+  const counter = 9;
+  const wrapper = setup(null, { counter });
+
+  // We find the increment button and we click it
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+
+  // Ask Gimeno why is this neccessary, the test still passes.
+  //wrapper.update();
+
+  // We check that the expected value is found in the resulting string
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
+
+/**
+ * These are the tests that are proposed in the challange part of Section 2.
+ */
+test('Renders decrement button', () => {
+  const wrapper = setup();
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  expect(decrementButton.length).toBe(1);
+});
+
+test('Clicking the decrement button decrements the counter display', () => {
+  // Setup the component with an initial counter of 4
+  const counter = 4;
+  const wrapper = setup(null, { counter });
+
+  // Find the decrement button and click it
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  decrementButton.simulate('click');
+
+  // Check that the counter display decremented.
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter - 1);
+});
+
+test('Clicking the decrement button when the counter is 0 shows the error message', () => {
+  // Setup the component with an initial state of 0 (this is the default, that's why we don't pass in any state)
+  const wrapper = setup();
+
+  // Expect the error element to not be present
+  const errorElement = findByTestAttr(wrapper, 'error-element');
+  expect(errorElement.length).toBe(0);
+
+  // Find the decrement button and click it
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  decrementButton.simulate('click');
+
+  // Search for the error element and expect it to be rendered.
+  const errorElementAfterClick = findByTestAttr(wrapper, 'error-element');
+  expect(errorElementAfterClick.length).toBe(1);
+});
+
+test('When the error message is visible clicking the increment button makes it dissapear', () => {
+  // Setup the component with it's initial counter value (0), and with the error flag to true
+  const wrapper = setup(null, { error: true });
+
+  // Check that the error message is there (remember that we set the error flag to true in the setup method)
+  const errorMessage = findByTestAttr(wrapper, 'error-element');
+  expect(errorMessage.length).toBe(1);
+
+  // Find and click the the increment button, afterwards check that the error message has disappeared
+  const incrementButton = findByTestAttr(wrapper, 'increment-button');
+  incrementButton.simulate('click');
+  const errorMessageAfterClick = findByTestAttr(wrapper, 'error-element');
+  expect(errorMessageAfterClick.length).toBe(0);
+});
+
+test('When the counter is something other than 0 and the reset button is clicked the counter display shows 0', () => {
+  // Set up the component with a counter value different from 0.
+  const wrapper = setup(null, { counter: 6, error: false });
+
+  // Find the reset button and click it.
+  const resetButton = findByTestAttr(wrapper, 'reset-button');
+  resetButton.simulate('click');
+
+  // Check that the counter display shows 0.
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain('0');
+});
+
+test('When the error message is present, if the reset button is clicked the message error disappears', () => {
+  // Set up the component with the error message present
+  const wrapper = setup(null, { error: true });
+
+  // Check that the error message is there
+  const errorMessage = findByTestAttr(wrapper, 'error-element');
+  expect(errorMessage.length).toBe(1);
+
+  // Find and click the reset button
+  const resetButton = findByTestAttr(wrapper, 'reset-button');
+  resetButton.simulate('click');
+
+  // Check that the message error has disappeared
+  const errorMessageAfterClick = findByTestAttr(wrapper, 'error-element');
+  expect(errorMessageAfterClick.length).toBe(0);
 });
